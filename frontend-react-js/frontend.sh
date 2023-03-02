42427af98b0e46d0fb8653786c14f185948cb1b2
iulianasdream <queen.iuliana.claudia@gmail.com>
2023-03-02 23:26:19 +0000

    week2 - frontend obs otel not working yet

--- frontend-react-js/frontend.sh
+++ frontend-react-js/frontend.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-npm install 
+npm install --save \
+    @opentelemetry/api \
+    @opentelemetry/sdk-trace-web \
+    @opentelemetry/exporter-trace-otlp-http \
+    @opentelemetry/context-zone \
+    @opentelemetry/auto-instrumentations-web \
+    @opentelemetry/instrumentation \
+    @opentelemetry/instrumentation-xml-http-request \
+    @opentelemetry/instrumentation-fetch
 npm start
--- frontend-react-js/src/index.js
+++ frontend-react-js/src/index.js
@@ -1,3 +1,4 @@
+import './tracing.js'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -11,6 +12,30 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+import { trace, context, } from '@opentelemetry/api';
+
+const tracer = trace.getTracer();
+
+const rootSpan = tracer.startActiveSpan('document_load', span => {
+  //start span when navigating to page
+  span.setAttribute('pageUrlwindow', window.location.href);
+  window.onload = (event) => {
+    // ... do loading things
+    alert("Your page is loaded");
+    // ... attach timing information
+    span.end(); //once page is loaded, end the span
+  };
+
+  button.clicked = (event) => {
+    tracer.startActiveSpan('app.button_clicked', btnSpan => {
+      // Add your attributes to describe the button clicked here
+      btnSpan.setAttribute('app.attr.signup', 'app.val.clicked');
+
+      btnSpan.end();
+    });
+  }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
